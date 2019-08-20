3f4da6f2e5b2512b36431aba8fe89f7327412f6d
Dawson <dawsonteu@gmail.com>
2019-08-20 00:11:46 -0400

    Deleted test files

--- src/canvasTest/index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // eslint-disable-next-line no-undef
-    const canvas = new Canvas(400, 400, 'a');
-
-    // eslint-disable-next-line no-undef
-    canvas.rect(100, 100, 100, 200, Canvas.Colour(255, 0, 0), 5, Canvas.Colour(0, 255, 0));
-});
