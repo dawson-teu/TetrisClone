2ed3c542b2b907b1b44ad01c6a954cf3d4ded1bc
Dawson <dawsonteu@gmail.com>
2019-09-05 20:00:00 -0400

    Setup project to work with CSS

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: path.resolve(__dirname, 'src/game.ts'),
@@ -9,23 +10,41 @@ module.exports = {
         filename: 'bundle.js',
     },
     resolve: {
-        extensions: [".ts", ".tsx", ".js", ".jsx"]
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
     },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                include: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'webpack.*')],
-                use: {
-                    loader: 'ts-loader'
-                }
+                include: [path.resolve(__dirname, 'src')],
+                use: ['ts-loader'],
             },
             {
                 test: /\.jsx?$/,
-                include: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'webpack.*')],
+                include: [path.resolve(__dirname, 'src')],
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.html$/,
+                include: [path.resolve(__dirname, 'src')],
+                use: ['html-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+                include: [path.resolve(__dirname, 'src')],
                 use: {
-                    loader: 'babel-loader',
+                    loader: 'file-loader',
+                    options: { name: '[path][name].[ext]', context: 'src/' },
                 },
+            },
+            {
+                test: /\.css$/,
+                include: [path.resolve(__dirname, 'src')],
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    { loader: 'css-loader', options: { sourceMap: true } },
+                    { loader: 'postcss-loader', options: { sourceMap: true } },
+                ],
             },
         ],
     },
@@ -34,6 +53,10 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'src/game.html'),
             filename: path.resolve(__dirname, 'build/game.html'),
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
         }),
     ],
 };
